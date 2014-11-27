a675c139fd06ed63252e7d27e009d58c7d775b24
Matt Pedersen <mpedersen2054@gmail.com>
2014-11-26 18:32:56 -0600

    add code for checkAnswer(), still having problem with correct answer submission

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-(function() {
+$(function() {
     var q1 = new Question(q1text, rawData[0], 'd'),
         q2 = new Question(q2text, rawData[1], 'a'),
         q3 = new Question(q3text, rawData[2], 'b');
@@ -19,25 +19,39 @@
                 ans = currentPos['answers'];
 
             $('.question h2').text(q);
-            // ansHtml from raw_data.js
-            $('.answers').html(ansHtml)
+            $('.answers').html(ansHtml) // ansHtml from rawData
                 .find('.answer').children('label')
                     .each(function(i) {
                         $(this).text(ans[i]);
                     })
 
+            GAME.checkAnswer(currentQuestion)
+        },
 
+        checkAnswer: function(i) {
+            $('#submit').on('click', function() {
+                var corrAns = GAME.questions[i]['correctAnswer'],
+                    subAns = $('.answer input[name="q"]:checked').val();
+
+                console.log(corrAns)
+
+                if (subAns != corrAns) {
+                    console.log('wrong!')
+                }
+
+                if (subAns == corrAns) {
+                    console.log('that is right!')
+                    GAME.numCorrect += 1;
+                    currentQuestion += 1;
+                    GAME.askQuestion(currentQuestion);
+                }
+            })
         }
     }
 
     GAME.initiate();
-    // GAME.askQuestion(0);
-    // GAME.checkAnswer(0);
-    // GAME.askQuestion(1);
-    // GAME.checkAnswer(1);
-    // GAME.askQuestion(2);
-    // GAME.checkAnswer(2);
-
+    var currentQuestion = 0;
+    GAME.askQuestion(0)
 
 
     function Question(question, answers, correctAnswer) {
